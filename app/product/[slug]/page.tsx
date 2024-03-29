d32ddf0fbd3d49bafd93d83218b8e395aba6bbfc
Mateo Sam <sammateo4@gmail.com>
2024-03-29 17:57:00 -0400

    product not found

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,5 +1,9 @@
 import { sanityClient } from "@/app/lib/sanity";
 import ProductImages from "@/components/products/ProductImages";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 let priceFormat = new Intl.NumberFormat("en-US", {
 	style: "currency",
@@ -37,7 +41,24 @@ export default async function page({ params }: { params: { slug: string } }) {
 					</div>
 				</div>
 			) : (
-				<div></div>
+				<div className="flex flex-col gap-y-5 h-[90vh] items-center justify-center">
+					<Image
+						className=" h-auto w-[50vw] md:w-[20vw]"
+						src={"/images/notfound.svg"}
+						width={0}
+						height={0}
+						alt="product not found"
+					/>
+					<p className="text-2xl">Product not found.</p>
+
+					<Link
+						href="/"
+						className="underline flex w-fit text-primary"
+					>
+						<ArrowLeft />
+						Back to home
+					</Link>
+				</div>
 			)}
 		</div>
 	);
